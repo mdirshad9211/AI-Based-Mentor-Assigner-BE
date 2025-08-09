c0b2992006f54cbf1d74d98427df33da8c8a4786
mdirshad1248 <irshad@bluparrot.in>
2025-08-09 20:33:55 +0530

    allowed all ips in cors

--- index.js
+++ index.js
@@ -21,20 +21,20 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173', // Vite dev server
-  'https://ai-based-mentor-assigner-fe.vercel.app'
+  'https://ai-based-mentor-assigner-fe.vercel.app/'
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
+   origin: "*",
+  //  function (origin, callback) {
+  //   if (!origin) return callback(null, true);
     
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  //   if (allowedOrigins.indexOf(origin) !== -1) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
