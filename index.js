edab9a9657a34cfc91a43a4fe8921ba9a65a4286
mdirshad1248 <irshad@bluparrot.in>
2025-08-09 11:48:50 +0530

    added cors

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ dotenv.config();
 
 
 const app = express();
-app.use(cors());
+// add frontend URL to CORS
+const allowedOrigins = ["https://ai-based-mentor-assigner-fe.vercel.app/", "http://localhost:3000"];
+app.use(cors({
+  origin: allowedOrigins
+}));
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
